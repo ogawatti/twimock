a5182301fcbbaca2f115df8a76862206c1cc378b
ogawa <z00s109031@gmail.com>
2014-12-22 11:39:40 +0900

    :memo: Added mock feature

--- lib/twimock.rb
+++ lib/twimock.rb
@@ -5,10 +5,46 @@ require "twimock/config"
 require "twimock/application"
 require "twimock/user"
 require "twimock/auth_hash"
+require "twimock/omniauth"
 require "twimock/errors"
 
 module Twimock
   extend self
+
+=begin
+  def on
+    ::Hoge.class_eval do
+      alias_method :__hoge, :hoge; remove_method :hoge
+      class << ::Hoge; self; end.class_eval do
+        alias_method :__foo, :foo; remove_method :foo
+      end
+      include ::Mock::Hoge
+    end
+  end
+
+  def off
+    ::Hoge.class_eval do
+      alias_method :hoge, :__hoge; remove_method :__hoge
+      class << ::Hoge; self; end.class_eval do
+        alias_method :foo,  :__foo;  remove_method :__foo
+      end
+    end
+  end
+
+  module Hoge
+    def hoge; p "hogeeeee"; end
+
+    module ClassMethods
+      def foo; p "fooooooo"; end
+    end
+
+    extend ClassMethods
+    
+    def self.included(klass)
+      klass.extend ClassMethods
+    end
+  end
+=end
 
   def auth_hash(access_token=nil)
     if access_token.kind_of?(String) && access_token.size > 0
