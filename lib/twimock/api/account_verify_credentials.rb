a0ce7c14a5d1990284e9edb857e564f31ba22452
RustyNail <takonoyawarakaage@yahoo.co.jp>
2015-02-02 14:15:24 +0900

    JSON形式のbodyを返却

--- lib/twimock/api/account_verify_credentials.rb
+++ lib/twimock/api/account_verify_credentials.rb
@@ -19,13 +19,12 @@ module Twimock
             raise if auth_header.blank?
             authorization = parse_authorization_header(auth_header.first)
             raise unless validate_authorization_header(authorization)
-            raise unless application = Twimock::Application.find_by_api_key(authorization.oauth_consumer_key)
-            raise unless user = Twimock::User.find_by_access_token(authorization.oauth_token)
+            raise unless @application = Twimock::Application.find_by_api_key(authorization.oauth_consumer_key)
+            raise unless @user = Twimock::User.find_by_access_token(authorization.oauth_token)
           rescue
             return unauthorized
           end
           status = '200 OK'
-          body = "{\"lang\":\"ja\"}"
           header = { "Content-Length" => body.bytesize }
           [ status, header, [ body ] ]
         else
@@ -39,6 +38,51 @@ module Twimock
         [ "401 Unauthorized", {}, "" ]
       end
 
+      def body
+        "{
+          \"id\":#{@user.id},
+          \"id_str\":\"#{@user.id}\",
+          \"name\":\"#{@user.name}\",
+          \"screen_name\":\"#{@user.name}\",
+          \"location\":\"\",
+          \"profile_location\":null,
+          \"description\":\"\",
+          \"url\":null,
+          \"entities\":{\"description\":{\"urls\":[]}},
+          \"protected\":false,
+          \"followers_count\":1,
+          \"friends_count\":1,
+          \"listed_count\":1,
+          \"created_at\":\"Wed Apr 27 00:00:00 +0000 2011\",
+          \"favourites_count\":1,
+          \"utc_offset\":32400,
+          \"time_zone\":\"Tokyo\",
+          \"geo_enabled\":true,
+          \"verified\":false,
+          \"statuses_count\":1,
+          \"lang\":\"ja\",
+          \"contributors_enabled\":false,
+          \"is_translator\":false,
+          \"is_translation_enabled\":false,
+          \"profile_background_color\":\"022330\",
+          \"profile_background_image_url\":\"http:\/\/abs.twimg.com\/images\/themes\/theme1\/bg.png\",
+          \"profile_background_image_url_https\":\"https:\/\/abs.twimg.com\/images\/themes\/theme1\/bg.png\",
+          \"profile_background_tile\":false,
+          \"profile_image_url\":\"http:\/\/abs.twimg.com\/sticky\/default_profile_images\/default_profile_0_normal.png\",
+          \"profile_image_url_https\":\"https:\/\/abs.twimg.com\/sticky\/default_profile_images\/default_profile_0_normal.png\",
+          \"profile_link_color\":\"0084B4\",
+          \"profile_sidebar_border_color\":\"A8C7F7\",
+          \"profile_sidebar_fill_color\":\"C0DFEC\",
+          \"profile_text_color\":\"333333\",
+          \"profile_use_background_image\":true,
+          \"default_profile\":false,
+          \"default_profile_image\":false,
+          \"following\":false,
+          \"follow_request_sent\":false,
+          \"notifications\":false
+        }".gsub!(/(^\s+|\n)/,'')
+      end
+
       def validate_authorization_header(authorization)
         return false unless authorization.oauth_consumer_key.size > 0
         return false unless authorization.oauth_nonce.size > 0
@@ -47,7 +91,6 @@ module Twimock
         return false unless authorization.oauth_timestamp.to_i > 0
         return false unless authorization.oauth_token.size > 0
         return false unless authorization.oauth_version == "1.0"
-
         true
       end
 
